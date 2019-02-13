51b03967840d7dba12f1fcd390275dfd8ee3af82
Meshack Mbuvi <meshmbuvi@gmail.com>
2019-02-13 19:07:02 +0300

    Admin code for the app

--- src/actions/client.tsx
+++ src/actions/client.tsx
@@ -1,24 +1,24 @@
-import axios from "axios";
-import { Redirect } from "react-router";
-import * as React from "react";
+import axios from 'axios';
+import { Redirect } from 'react-router';
+import * as React from 'react';
 
 const client = axios.create({
-  baseURL: "http://localhost:3001/api"
+	baseURL: 'https://twiter-clone-api.herokuapp.com/api',
 });
 
-const token = window.localStorage.getItem("token");
+const token = window.localStorage.getItem('token');
 client.defaults.headers.common.authorization = `Bearer ${token}`;
 
 client.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response.status === 401) {
-      //   remove token since its expired
-      localStorage.setItem("token", "");
-      return <Redirect to="/" />;
-    }
-    return Promise.reject(error);
-  }
+	response => response,
+	error => {
+		if (error.response.status === 401) {
+			//   remove token since its expired
+			localStorage.setItem('token', '');
+			return <Redirect to="/" />;
+		}
+		return Promise.reject(error);
+	}
 );
 
 export default client;
--- src/actions/messages.tsx
+++ src/actions/messages.tsx
@@ -1,17 +1,19 @@
-import client from "./client";
-import { SET_MESSAGES } from "./types";
+import client from './client';
+import { SET_MESSAGES } from './types';
+
+// const BASE_URL = process.env.BASE_URL;
 export const getAllMessages = () => async (dispatch: any) => {
-  try {
-    const res = await client.get("/messages");
-    dispatch(setMessages(res.data));
-  } catch (error) {
-    console.log("err ", error);
-  }
+	try {
+		const res = await client.get(`/messages`);
+		dispatch(setMessages(res.data));
+	} catch (error) {
+		console.log('err ', error);
+	}
 };
 
 export const setMessages = (payload: any) => {
-  return {
-    type: SET_MESSAGES,
-    payload
-  };
+	return {
+		type: SET_MESSAGES,
+		payload,
+	};
 };
